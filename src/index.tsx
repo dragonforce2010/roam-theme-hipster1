75621ab5de68549abeadd4c9f2cfaa898247f3f0
dragonforce2010 <coding.monkey.github@gmail.com>
2023-03-13 02:24:27 +0800

    new theme

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import './index.css'
+
+
+
+function onload() {
+  document.body.className = 'hipster1'
+}
+
+function onunload() {
+
+}
+
+export default {
+  onload: onload,
+  onunload: onunload,
+}
